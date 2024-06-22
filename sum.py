7b22581ba8dd1b22f4aa6d7ed1a7458084169397
SahanaAshokan123 <sanusanu17888@gmail.com>
2024-06-22 18:53:12 +0530

    first file

---
+++ sum.py
@@ -0,0 +1,11 @@
+# Prompt the user to enter the first number
+number1 = float(input("Enter the first number: "))
+
+# Prompt the user to enter the second number
+number2 = float(input("Enter the second number: "))
+
+# Add the numbers
+sum = number1 + number2
+
+# Print the result
+print("The sum of", number1, "and", number2, "is", sum)
